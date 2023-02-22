ac6777ed674161acdf44733430cfa20723538f58
Danny Tram <tramnhatquang@gmail.com>
2023-02-21 18:32:24 -0800

    added files

--- 713. Subarray Product Less Than K.py
+++ 713. Subarray Product Less Than K.py
@@ -1,20 +1,37 @@
 class Solution:
-	def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-		if k <= 1:
-			return 0
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
 
-		# use the sliding window technique
-		left = ans = 0
-		curr_product = 1
+        # use the sliding window technique
+        left = ans = 0
+        curr_product = 1
 
-		for right in range(len(nums)):
-			curr_product *= nums[right]
-			while curr_product >= k:
-				curr_product //= nums[left]
-				left += 1
+        for right in range(len(nums)):
+            curr_product *= nums[right]
+            while curr_product >= k:
+                curr_product //= nums[left]
+                left += 1
 
-			# the trick part is that the newly created arr for each index is equal to right - left + 1 which is window length
-			ans += right - left + 1
+            # the trick part is that the newly created arr for each index is equal to right - left + 1 which is window length
+            ans += right - left + 1
 
-		return ans
-		# time: O(n), space: O(1), n is the length of nums
+        return ans
+        # time: O(n), space: O(1), n is the length of nums
+
+    def numSubarrayProductLessThanK_brute_force(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        count = 0
+        for i in range(n):
+            product = 1
+            for j in range(i, n):
+                product *= nums[j]
+                if product < k:
+                    count += 1
+        return count
+        """
+        Brute force solution: Find all subarrays whose product is strictly less than k, then increase the count by 1
+
+        """
+        # time: O(n^2)
+        # space: O(1)
--- 82. Remove Duplicates from Sorted List II.py
+++ 82. Remove Duplicates from Sorted List II.py
@@ -4,21 +4,22 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-	def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-		# APPROACH 1: Using a sentinel head + predecessor
-		prev = dummy = ListNode(0, head)
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        currHead = head
+        dummy = prev = ListNode(0, head)
 
-		# move the head pointer if the next value is equal to the current value
-		while head and head.next:
-			if head.val == head.next.val:
-				while head.next and head.val == head.next.val:
-					head = head.next
-				# head will point to the last element in the duplicate sequence
-				head = head.next
-				# connect the prev's next to the head
-				prev.next = head
+        # We will traverse the original list and skip all the duplicates since duplicates are consecutive in sorted order. When we reach a distinct node, connect curr.next = currHead and move the curr to the next pointer.
+        # We keep doing that until the currHead is None
+        while currHead:
+            if currHead.next and currHead.next.val == currHead.val:
+                while currHead.next and currHead.next.val == currHead.val:
+                    currHead = currHead.next
+                # connect the last distinct node to the next distinct node
+                prev.next = currHead.next
+            else:
+                prev = prev.next
+            currHead = currHead.next
+        return dummy.next
 
-			else:
-				prev = prev.next
-				head = head.next
-		return dummy.next
+        # time: O(n), n is length of arr
+        # spacE: O(1)
